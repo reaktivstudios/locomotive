1270c89fbe37bab8d2b80fbe7aec1c2b1f29f9e3
Zach Wills <zachwills@live.com>
2015-10-02 16:20:31 -0400

    Getting admin page set up.

---
+++ batch-processing.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Plugin Name: Batch Processing
+ * Version: 0.1.0
+ * Description: Enables developers to utilize an abstract class to achieve easy batch processes that can be run through WP Admin or (if installed) WP CLI.
+ * Author: Reaktiv Studios
+ * Author URI: http://reaktivstudios.com/
+ * License: GPL
+ *
+ * @package  Batch_Process
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Plugin class to handle setting constants and loading files and static helper methods.
+ */
+final class Batch_Processing {
+	/**
+	 * Define all the constants we need
+	 */
+	public function define_constants() {
+		define( 'BATCH_VERSION', '0.1.0-dev' );
+		define( 'BATCH_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+		define( 'BATCH_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		define( 'BATCH_PLUGIN_FILE', __FILE__ );
+	}
+
+	/**
+	 * Load in all the files we need.
+	 */
+	public function load_includes() {
+		require_once( BATCH_PLUGIN_DIR . '/includes/class-batch.php' );
+	}
+
+	/**
+	 * Handle hooks.
+	 */
+	public function attach_hooks() {
+		add_action( 'admin_menu', array( $this, 'dashboard' ) );
+	}
+
+	/**
+	 * Dashboard.
+	 */
+	public function dashboard() {
+		add_menu_page(
+			'Batch Processes',
+			'Batch Processes',
+			'manage_options',
+			'batch-processes',
+			array( $this, 'dashboard_display' )
+		);
+	}
+
+	/**
+	 * Dashboard display.
+	 */
+	public function dashboard_display() {
+		?>
+		<div class="wrap">
+			<h2><?php esc_html_e( get_admin_page_title() ); ?></h2>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Init.
+	 */
+	public function init() {
+		$this->define_constants();
+		$this->load_includes();
+		$this->attach_hooks();
+	}
+}
+
+$batch_processing = new Batch_Processing();
+$batch_processing->init();
