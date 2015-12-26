2bc5ef24ca18f3793d8aabb1752715bd6aaf330a
Zach Wills <zachwills@live.com>
2015-12-26 11:47:19 -0500

    Add Gulp to the project and get basic react working

---
+++ assets/src/js/main.js
@@ -0,0 +1,8 @@
+// main.js
+var React = require( 'react' );
+var ReactDOM = require( 'react-dom' );
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById( 'example' )
+);
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require( 'gulp' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+var react = require( 'gulp-react' );
+
+var path = {
+    JS: [
+        'assets/src/js/*.js',
+        'assets/src/js/**/*.js'
+    ],
+    MINIFIED_OUT: 'build.min.js',
+    DEST: 'assets'
+};
+
+gulp.task( 'build', function() {
+    gulp.src( path.JS )
+        .pipe( react() )
+        .pipe( concat( path.MINIFIED_OUT ) )
+        .pipe( uglify() )
+        .pipe( gulp.dest( path.DEST ) );
+});
+
+gulp.task('watch', function(){
+    gulp.watch( path.JS, [ 'build' ] );
+});
+
+gulp.task( 'default', [ 'watch' ] );
