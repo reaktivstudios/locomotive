80193e256e0addb8b6dc9f55ceeffce2c240a036
Zach Wills <zachwills@live.com>
2016-01-07 03:11:52 -0500

    Update react state to reflect currently selected batch

--- assets/src/js/batch.jsx
+++ assets/src/js/batch.jsx
@@ -5,12 +5,29 @@ var ReactDOM = require( 'react-dom' );
  * Our Batch Processing App.
  */
 var App = React.createClass( {
-    getInitialState: function() {
+    getInitialState : function() {
         return {
             // Batches that can be run. Loaded through `wp_localize_script()`.
             batches: batch.batches,
             page_title: batch.page_title,
+
+            // Object to hold data relating to running a migration (in the modal).
+            processing: {
+                active: false,
+                batch: false
+            }
         };
+    },
+
+    /**
+     * Update which batch is currently selected.
+     *
+     * @param key Currently selected batch key.
+     */
+    updateSelectedBatch : function( key ) {
+        // @todo Only allow this action if we are not currently processing something.
+        this.state.processing.batch = key;
+        this.setState( { processing: this.state.processing } );
     },
 
     /**
@@ -22,7 +39,11 @@ var App = React.createClass( {
         return (
             <div className="wrap">
                 <h2>{ this.state.page_title }</h2>
-                <BatchPicker batches={ this.state.batches } />
+                <BatchPicker
+                    batches={ this.state.batches }
+                    processing={ this.state.processing }
+                    updateSelectedBatch={ this.updateSelectedBatch }
+                />
             </div>
         )
     }
@@ -41,7 +62,7 @@ var BatchPicker = React.createClass( {
     renderBatchOption : function( key ) {
         var batch = this.props.batches[ key ];
         return (
-            <li key={ key }>
+            <li key={ key } onClick={ this.props.updateSelectedBatch.bind( null, key ) }>
                 <input type="radio" id={ key } name="batch_process" className="batch-process-option" value="test-another-batch" />
                     <label htmlFor={ key }>
                         { batch.name } <small>last run: { batch.last_run } | status: { batch.status }</small>
@@ -62,8 +83,8 @@ var BatchPicker = React.createClass( {
                     { Object.keys( this.props.batches ).map( this.renderBatchOption ) }
                 </ul>
 
-                <button id="submit" className="button button-primary">Run Batch Process</button>
-                <button id="reset" className="button button-secondary">Reset Batch Process</button>
+                <button id="submit" className="button button-primary" onClick={ this.run }>Run Batch Process</button>
+                <button id="reset" className="button button-secondary" onClick={ this.reset }>Reset Batch Process</button>
             </div>
         )
     }
