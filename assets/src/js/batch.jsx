188b317d15cc7c4e3e9be794de6e1ef66bf4900b
Zach Wills <zachwills@live.com>
2016-01-13 12:44:59 -0500

    Show errors if they occur

--- assets/src/js/batch.jsx
+++ assets/src/js/batch.jsx
@@ -22,7 +22,8 @@ var App = React.createClass( {
                 // batch process..
                 remote_data: {
                     batch_title: '',
-                    status: 'Starting',
+                    status: '',
+                    error: '',
                     progress: 0,
                     current_step: 0,
                     total_steps: 0,
@@ -69,49 +70,48 @@ var App = React.createClass( {
         var _this = this,
             batch_slug = _this.state.processing.batch.toString();
 
-        if ( 1 === current_step ) {
-            // Starting batch process.
-        }
-
         $.ajax( {
             type: 'POST',
             url: batch.ajaxurl,
             data: {
                 batch_process: batch_slug,
-                nonce: batch.nonce,
-                step: current_step,
-                action: 'run_batch',
+                nonce:         batch.nonce,
+                step:          current_step,
+                action:        'run_batch',
             },
             dataType: 'json',
             success: function( response ) {
-                // Update our state with the processing status and progress.
+                // Update our state with the processing status and progress, which will update the modal.
+                _this.state.processing.remote_data = {
+                    batch_title:       response.batch,
+                    status:            response.status,
+                    progress:          response.progress,
+                    current_step:      response.current_step,
+                    total_steps:       response.total_steps,
+                    total_num_results: response.total_num_results
+                };
+
+                // Update our batches, which will update the batch listing.
+                _this.state.batches[ _this.state.processing.batch ].last_run = 'just ran';
+                _this.state.batches[ _this.state.processing.batch ].status = _this.state.processing.remote_data.status;
+
+                // Check for errors.
+                if ( response.error ) {
+                    _this.state.processing.remote_data.error = response.error;
+                    _this.setState( { processing: _this.state.processing } );
+                }
+
+                _this.setState( {
+                    processing: _this.state.processing,
+                    batches: _this.state.batches
+                } );
+
+                // Determine if we have to run another step in the batch. Checks if there are more steps
+                // that need to run and makes sure the 'status' from the server is still 'running'.
                 if ( response.success ) {
-                    _this.state.processing.remote_data = {
-                        batch_title:       response.batch,
-                        status:            response.status,
-                        progress:          response.progress,
-                        current_step:      response.current_step,
-                        total_steps:       response.total_steps,
-                        total_num_results: response.total_num_results
-                    };
-
-                    _this.state.batches[ _this.state.processing.batch ].last_run = 'just ran';
-                    _this.state.batches[ _this.state.processing.batch ].status = _this.state.processing.remote_data.status;
-
-                    _this.setState( {
-                        processing: _this.state.processing,
-                        batches: _this.state.batches
-                    } );
-
-                    //
                     if ( response.current_step !== response.total_steps && 'running' === response.status.toLowerCase() ) {
                         _this.runBatch( current_step + 1 );
-                    } else {
-                        // Make sure the select list is updated with the right text.
                     }
-                } else {
-                    // Update the modal with response.error.
-                    alert( response.error );
                 }
             }
         } ).fail( function ( response ) {
@@ -131,6 +131,11 @@ var App = React.createClass( {
      * @returns {JSX}
      */
     render : function() {
+        var selectedBatch = '';
+        if ( this.state.processing.batch ) {
+            selectedBatch = this.state.batches[ this.state.processing.batch ].name;
+        }
+
         return (
             <div className="wrap">
                 <h2>{ this.state.page_title }</h2>
@@ -143,6 +148,7 @@ var App = React.createClass( {
 
                 <Modal
                     isOpen={ this.state.processing.active }
+                    selectedBatch={ selectedBatch }
                     batchInfo={ this.state.processing.remote_data }
                     toggleProcessing={ this.toggleProcessing }
                 />
@@ -198,26 +204,59 @@ var BatchPicker = React.createClass( {
  */
 var Modal = React.createClass( {
     render : function() {
-        var classes = 'batch-processing-overlay',
-            batch_info = this.props.batchInfo;
+        var classes        = 'batch-processing-overlay',
+            batch_info     = this.props.batchInfo,
+            batch_title    = ( batch_info.batch_title ) ? batch_info.batch_title : this.props.selectedBatch,
+            status         = ( batch_info.status ) ? batch_info.status : '',
+            progress_style = {
+                width: batch_info.progress + '%'
+            };
 
         if ( this.props.isOpen ) {
             classes += ' is-open';
         }
 
-        var progress_style = {
-            width: batch_info.progress + '%'
+        /**
+         * Return the title for the modal.
+         *
+         * @returns {JSX}
+         */
+        var title = function() {
+            if ( status ) {
+                return <h2>{ batch_title }: { status }</h2>;
+            } else {
+                return <h2>{ batch_title }</h2>
+            }
+        }
+
+        /**
+         * Return content for the modal.
+         *
+         * @returns {JSX}
+         */
+        var content = function() {
+            if ( batch_info.error ) {
+                return (
+                    <div className="batch-error">
+                        { batch_info.error }
+                    </div>
+                );
+            } else {
+                return (
+                    <div className="progress-bar">
+                        <span className="progress-bar__text">Progress: { batch_info.progress }%</span>
+                        <div className="progress-bar__visual" style={ progress_style }></div>
+                    </div>
+                );
+            }
         }
 
         return (
             <div className={ classes }>
                 <div className="close" onClick={ this.props.toggleProcessing.bind( null, false ) }>close</div>
                 <div className="batch-overlay__inner">
-                    <h2>{ batch_info.batch_title }: { batch_info.status }</h2>
-                    <div className="progress-bar">
-                        <span className="progress-bar__text">Progress: { batch_info.progress }%</span>
-                        <div className="progress-bar__visual" style={ progress_style }></div>
-                    </div>
+                    { title() }
+                    { content() }
                 </div>
             </div>
         );
