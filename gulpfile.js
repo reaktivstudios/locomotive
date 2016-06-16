962c86694848b575017c73bdad62381219c05b29
Zach Wills <zachwills@live.com>
2016-06-16 09:24:49 -0400

    Appease JSCS gods.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 // Props to http://mikevalstar.com/post/fast-gulp-browserify-babelify-watchify-react-build/ for most of this file.
 'use strict';
 
-var gulp = require('gulp');  // Base gulp package.
-var babelify = require('babelify'); // Used to convert ES6 & JSX to ES5.
-var browserify = require('browserify'); // Providers "require" support, CommonJS.
-var notify = require('gulp-notify'); // Provides notification to both the console and Growel.
-var rename = require('gulp-rename'); // Rename sources.
-var sourcemaps = require('gulp-sourcemaps'); // Provide external sourcemap files.
-var livereload = require('gulp-livereload'); // Livereload support for the browser.
-var gutil = require('gulp-util'); // Provides gulp utilities, including logging and beep.
-var chalk = require('chalk'); // Allows for coloring for logging.
-var source = require('vinyl-source-stream'); // Vinyl stream support.
-var buffer = require('vinyl-buffer'); // Vinyl stream support.
-var watchify = require('watchify'); // Watchify for source changes.
-var merge = require('utils-merge'); // Object merge tool.
-var duration = require('gulp-duration'); // Time aspects of your gulp process.
-var uglify = require('gulp-uglify'); // Minify the JS.
+var gulp = require( 'gulp' );  // Base gulp package.
+var babelify = require( 'babelify' ); // Used to convert ES6 & JSX to ES5.
+var browserify = require( 'browserify' ); // Providers "require" support, CommonJS.
+var notify = require( 'gulp-notify' ); // Provides notification to both the console and Growel.
+var rename = require( 'gulp-rename' ); // Rename sources.
+var sourcemaps = require( 'gulp-sourcemaps' ); // Provide external sourcemap files.
+var livereload = require( 'gulp-livereload' ); // Livereload support for the browser.
+var gutil = require( 'gulp-util' ); // Provides gulp utilities, including logging and beep.
+var chalk = require( 'chalk' ); // Allows for coloring for logging.
+var source = require( 'vinyl-source-stream' ); // Vinyl stream support.
+var buffer = require( 'vinyl-buffer' ); // Vinyl stream support.
+var watchify = require( 'watchify' ); // Watchify for source changes.
+var merge = require( 'utils-merge' ); // Object merge tool.
+var duration = require( 'gulp-duration' ); // Time aspects of your gulp process.
+var uglify = require( 'gulp-uglify' ); // Minify the JS.
 
 // Configuration for Gulp.
 var config = {
@@ -23,18 +23,15 @@ var config = {
         src: 'assets/src/js/batch.jsx',
         watch: 'assets/src/js/**/*',
         outputDir: 'assets/dist/',
-        outputFile: 'batch.min.js',
-    },
+        outputFile: 'batch.min.js'
+    }
 };
 
 /**
  * Task to handle the JavaScript building and sourcemap generating.
  */
 gulp.task( 'javascript', function() {
-    livereload.listen(); // Start livereload server.
-
     var args = merge( watchify.args, { debug: true } ); // Merge in default watchify args with browserify arguments.
-
     var bundler = browserify( config.js.src, args ) // Browserify.
         .plugin( watchify, {
             ignoreWatch: ['**/node_modules/**', '**/bower_components/**']
@@ -42,6 +39,8 @@ gulp.task( 'javascript', function() {
         .transform( babelify, {
             presets: ['es2015', 'react']
         } );
+
+    livereload.listen(); // Start livereload server.
 
     bundle( bundler ); // Run the bundle the first time (required for Watchify to kick in).
 
@@ -56,8 +55,10 @@ function bundle( bundler ) {
 
     return bundler
         .bundle()
-        .on('error', function(err) { console.error(err); this.emit('end'); })
-        .pipe( source( 'batch.jsx' )) // Set source name.
+        .on( 'error', function( err ) {
+ console.error( err ); this.emit( 'end' );
+ })
+        .pipe( source( 'batch.jsx' ) ) // Set source name.
         .pipe( buffer() ) // Convert to gulp pipeline.
         .pipe( rename( config.js.outputFile ) ) // Rename the output file.
         .pipe( sourcemaps.init( { loadMaps: true } ) ) // Extract the inline sourcemaps.
@@ -65,7 +66,7 @@ function bundle( bundler ) {
         .pipe( sourcemaps.write( './map' ) ) // Set folder for sourcemaps to output to.
         .pipe( gulp.dest( config.js.outputDir ) ) // Set the output folder.
         .pipe( notify( {
-           message: 'Generated file: <%= file.relative %>',
+           message: 'Generated file: <%= file.relative %>'
         } ) ) // Output the file being created.
         .pipe( bundleTimer ) // Output time timing of the file creation.
         .pipe( livereload() ); // Reload the view in the browser.
