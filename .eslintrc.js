898a0c17da2ebaba04ef88c8e035f8c34263a9f7
Jay Hoffmann <jhoffm34@gmail.com>
2016-12-07 16:10:39 -0500

    Adding WP-like config rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,22 +20,37 @@ module.exports = {
       "batch": true
     },
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "react/no-direct-mutation-state" : "off"
+        "array-bracket-spacing": [ "error", "always" ],
+        "brace-style" : [ "error", "1tbs" ],
+        "camelcase": "error",
+        "computed-property-spacing": [ "error", "always" ],
+        "curly" : "error",
+        "eol-last": "error",
+        "eqeqeq": "error",
+        "indent": [ 1, "tab" ],
+        "keyword-spacing": "error",
+        "linebreak-style": [ "error", "unix" ],
+        "no-caller": "error",
+        "no-eq-null": "error",
+        "no-else-return": "warn",
+        "no-mixed-spaces-and-tabs": [1, "smart-tabs"],
+        "no-multi-spaces": "error",
+        "no-nested-ternary": "error",
+        "no-shadow": "error",
+        "no-spaced-func": "error",
+        "no-trailing-spaces": "error",
+        "no-unused-expressions": "error",
+        "no-unused-vars": "error",
+        "object-curly-spacing": [ "error", "always" ],
+        "quotes": [ "error", "single" ],
+        "react/no-direct-mutation-state" : "off",
+        "react/jsx-curly-spacing": [ 1, "always" ],
+        "react/jsx-space-before-closing" : "warn",
+        "semi": [ "error", "always" ],
+        "semi-spacing": "warn",
+        "space-before-blocks" : "error",
+        "space-before-function-paren" : "error",
+        "space-in-parens" : [ "error", "always" ],
+        "wrap-iife": [ "error", "any" ]
     }
 };
