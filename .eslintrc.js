7e79d1d0d6678ffd7e66488baa33ac8f61208b78
Jay Hoffmann <jhoffm34@gmail.com>
2016-12-07 13:54:23 -0500

    Addresses #41, Adds ESLint config to gulp
    Eslint has been added to gulp to ensure enforcement. Follows a standard set of rules from ESLint and React, with the one exception being the exclusion of the direct mutation state rule from React turned off, since this is not being followed. ESLint is built into gulp, and will run on build, but will not block the build process for now. JSCS has merge with ESLint so this addresses both issues

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "globals": {
+      "jQuery": true,
+      "require": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "react/no-direct-mutation-state" : "off"
+    }
+};
