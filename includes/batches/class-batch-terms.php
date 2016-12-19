b8b6ac07f37b574bbb9fc92a46620ecc4f81e394
Jay Hoffmann <jhoffm34@gmail.com>
2016-12-19 15:07:16 -0500

    Fixing Term_Query

--- includes/batches/class-batch-terms.php
+++ includes/batches/class-batch-terms.php
@@ -41,7 +41,7 @@ class Terms extends Batch {
 
 		// Need to do a count query in order to get all possible terms as an integer.
 		$count_args = wp_parse_args( array( 'fields' => 'count', 'offset' => 0 ), $this->args );
-		$count = new WP_Term_query( $count_args );
+		$count = new WP_Term_Query( $count_args );
 		$this->total_num_results = $count->get_terms();
 
 		return $query->get_terms();
